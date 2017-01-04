ecacb1c3e6729e35110aaf94cdd4dae77566d870
Ninad Ingole <needna78@gmail.com>
2017-01-04 11:05:47 +0530

    Test
    Add first test as a placeholder

---
+++ src/test/java/unittest/AppTest.java
@@ -0,0 +1,17 @@
+package unittest;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+/**
+ * Created by Ninad on 1/4/2017.
+ */
+public class AppTest {
+
+    @Test
+    public void test(){
+        fail("Test needs to be done");
+    }
+
+}
